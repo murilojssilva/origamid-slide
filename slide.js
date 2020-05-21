d1ff0c2959259ab749f71d1d50b0526107436ed4
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2020-05-21 15:34:54 -0300

    Event calc distances

--- slide.js
+++ slide.js
@@ -2,14 +2,32 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.distance = {
+      finalPosition: 0,
+      startX: 0,
+      moviment: 0,
+    };
+  }
+  moveSlide(distanceX) {
+    this.distance.movePosition = distanceX;
+    this.slide.style.transform = `transition3d(${distanceX}px,0,0)`;
+  }
+  updatePosition(clientX) {
+    this.distance.moviment = (this.distance.startX - clientX) * 1.6;
+    return this.distance.finalPosition - this.distance.moviment;
   }
   onStart(e) {
     e.preventDefault();
+    this.distance.startX = e.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
-  onMove(e) {}
+  onMove(e) {
+    const finalPosition = this.updatePosition(e.clientX);
+    this.moveSlide(finalPosition);
+  }
   onEnd(e) {
     this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.distance.finalPosition = this.distance.movePosition;
   }
   addSlideEvent() {
     this.wrapper.addEventListener("mousedown", this.onStart);
